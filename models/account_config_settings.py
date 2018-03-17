ea37e8b64ab4ec75a8802b93774e7921ae5301c4
Ruben Bravo <rbravo@ruben>
2018-03-17 07:02:17 -0430

    cambio para probar en la v11

--- models/account_config_settings.py
+++ models/account_config_settings.py
@@ -6,9 +6,9 @@ from odoo import fields, models
 from ..models.account_invoice import GROUP_AICT
 
 
-class AccountConfigSettings(models.TransientModel):
+class ResConfigSettings(models.TransientModel):
 
-    _inherit = 'account.config.settings'
+    _inherit = 'res.config.settings'
 
     group_supplier_inv_check_total = fields.Boolean(
         string="Check Total on Vendor Bills",
